f630adc337bcc28d97a7b981cc3be971dcd627a3
mohamad hosein <mmdtrni@gmail.com>
2021-08-08 23:44:02 +0430

    general structure only

---
+++ Maktab_Flask_project/WebBlog/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.logger.debug(f'app.instance_path = {app.instance_path}')
+    app.config.from_mapping(
+        SECRET_KEY='.jHETCR4ER*@V{/'
+
+    )
+    return app
