f1f014e8a3525048d9f4e8b36459f4771466fdec
mohammadmgh <mohammad.m.ghasemloo@gmail.com>
2021-08-10 13:27:30 +0430

    some changes and then creating login inteface

--- Maktab_Flask_project/WebBlog/blog.py
+++ Maktab_Flask_project/WebBlog/blog.py
@@ -6,3 +6,7 @@ blog_bp = Blueprint('blog', __name__)
 @blog_bp.route('/')
 def home():
     return render_template('home.html')
+
+@blog_bp.route("/login/")
+def login():
+    return render_template('login.html')
