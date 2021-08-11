7dbab281db9989dd792ef7404b389cd1e7e9a06e
surgecoder <58151523+surgecoder@users.noreply.github.com>
2021-08-11 23:03:24 +0430

    Update __init__.py

--- Maktab_Flask_project/WebBlog/__init__.py
+++ Maktab_Flask_project/WebBlog/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.logger.debug(f'app.instance_path = {app.instance_path}')
     app.config.from_mapping(
         SECRET_KEY='.jHETCR4ER*@V{/'
