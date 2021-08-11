6779c98d85782c4f3699d6be189abb00eab1bff7
surgecoder <58151523+surgecoder@users.noreply.github.com>
2021-08-11 21:06:45 +0430

    Update __init__.py
    just adding test_confiq=None and , instance_relative_config=True ...

--- Maktab_Flask_project/WebBlog/__init__.py
+++ Maktab_Flask_project/WebBlog/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
     app.logger.debug(f'app.instance_path = {app.instance_path}')
     app.config.from_mapping(
         SECRET_KEY='.jHETCR4ER*@V{/'
